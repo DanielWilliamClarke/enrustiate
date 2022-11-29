5b73f126ac6587dce6481f825c4c09cbbcc8b3be
Daniel William Clarke <dan.clarke@loveholidays.com>
2022-11-29 09:42:13 +0000

    can now render numbers up to 1 quadrillion

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,7 @@ fn main() {
     let args: Vec<String> = env::args().collect::<Vec<String>>();
     let input = args[1].clone();
 
-    match NumberToWords::validate(input, 0, 1_000_000) {
+    match NumberToWords::<i64>::validate(input, 0, 1_000_000_000_000_000) {
         Ok(renderer) => print!("Output: {}", renderer),
         Err(err) => println!("Error: {}", err),
     }
--- src/numbers_to_words.rs
+++ src/numbers_to_words.rs
@@ -1,10 +1,10 @@
-use std::{fmt::{Display}, str::FromStr};
+use std::{fmt::Display, str::FromStr};
 
-use crate::{validator::Validator, validation_errors::InputError};
+use crate::{validation_errors::InputError, validator::Validator};
 
 pub struct NumberToWords<N>(N);
 
-impl<N> NumberToWords<N>  {
+impl<N> NumberToWords<N> {
     fn new(input: N) -> Self {
         NumberToWords(input)
     }
@@ -18,7 +18,11 @@ where
     type Output = NumberToWords<N>;
     type Error = InputError<N>;
 
-    fn validate(input: String, low: Self::Bounds, high: Self::Bounds) -> Result<Self::Output, Self::Error> {
+    fn validate(
+        input: String,
+        low: Self::Bounds,
+        high: Self::Bounds,
+    ) -> Result<Self::Output, Self::Error> {
         match input.parse::<N>() {
             Err(_) => Err(InputError::ParseError(input.clone())),
             Ok(val) if val < low || val > high => Err(InputError::ValidationError(val)),
@@ -27,7 +31,35 @@ where
     }
 }
 
-impl Display for NumberToWords<i32> {
+impl NumberToWords<i64> {
+    fn render(
+        &self,
+        f: &mut std::fmt::Formatter<'_>,
+        mod_val: i64,
+        delim_val: i64,
+        mag: &str,
+    ) -> std::fmt::Result {
+        let mod_mag = self.0 % mod_val;
+        if mod_mag == 0 {
+            return write!(f, "{} {}", NumberToWords::new(self.0 / mod_val), mag);
+        }
+
+        let mut delim = "";
+        if mod_mag < delim_val {
+            delim = "and ";
+        }
+
+        write!(
+            f,
+            "{} {}{}",
+            NumberToWords::new(self.0 - mod_mag),
+            delim,
+            NumberToWords::new(mod_mag)
+        )
+    }
+}
+
+impl Display for NumberToWords<i64> {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self.0 {
             0 => write!(f, "zero"),
@@ -60,42 +92,42 @@ impl Display for NumberToWords<i32> {
             90 => write!(f, "ninety"),
             21..=99 => {
                 let mod_ten = self.0 % 10;
-                write!(f, "{} {}",
+                write!(
+                    f,
+                    "{} {}",
                     NumberToWords::new(self.0 - mod_ten),
                     NumberToWords::new(mod_ten)
                 )
-            },
+            }
             100..=999 => {
                 let mod_hundred = self.0 % 100;
                 if mod_hundred == 0 {
-                    return write!(f, "{} hundred", NumberToWords::new(self.0 / 100))
+                    return write!(f, "{} hundred", NumberToWords::new(self.0 / 100));
                 }
-                
-                write!(f, "{} and {}",
+
+                write!(
+                    f,
+                    "{} and {}",
                     NumberToWords::new(self.0 - mod_hundred),
                     NumberToWords::new(mod_hundred)
                 )
-            },
+            }
             1000..=9_99_999 => {
-                let mod_thousand = self.0 % 1000;
-                if mod_thousand == 0 {
-                    return write!(f, "{} thousand", NumberToWords::new(self.0 / 1000))
-                }
-                
-                let mut delim = ""; 
-                if mod_thousand < 100 {
-                    delim = "and ";
-                }
-                
-                write!(f, "{} {}{}",
-                    NumberToWords::new(self.0 - mod_thousand),
-                    delim,
-                    NumberToWords::new(mod_thousand)
-                )
-            },
-            1_000_000 =>  write!(f, "{} million", NumberToWords::new(self.0 / 1_000_000)),
-            _ => write!(f, "Unknown number")
+                self.render(f, 1000, 100, "thousand")
+            }
+            1_000_000..=999_999_999 => {
+                self.render(f, 1_000_000, 1000, "million")
+            }
+            1_000_000_000..=999_999_999_999 => {
+                self.render(f, 1_000_000_000, 1_000_000, "billion")
+            }
+            1_000_000_000_000..=999_999_999_999_999 => {
+                self.render(f, 1_000_000_000_000, 1_000_000_000, "trillion")
+            }
+            1_000_000_000_000_000 => {
+                self.render(f, 1_000_000_000_000_000, 1_000_000_000_000, "quadrillion")
+            }
+            _ => write!(f, "Invalid number"),
         }
     }
 }
-
